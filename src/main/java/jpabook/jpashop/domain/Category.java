d6d1d42e38572080e118d100169f04d041fbc800
bkdoo <kyeongdoos@gmail.com>
2022-06-21 00:03:51 +0900

    20220620 JPA 실습 - Entity Class 수정  - cascade설정, 지연관계설정, 연관관계 편의 메소드 설정 - 엔티티 개발시 주의사항 학습

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Getter @Setter
 public class Category {
@@ -25,10 +27,16 @@ public class Category {
     ) // 다대다의 경우 중간맵핑 테이블을 구성해야함, 중간테이블의 조인컬럼 지정, item_id와도 맵핑, 한쪽만 지정해두면됨(반대는 mapped by)
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child;
+
+    // 연관관계 편의 메소드 //
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter @Setter
 @Table(name = "orders")
@@ -17,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL, fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery; // 일대일일경우 FK를 아무 곳에나 둬도 문제 없지만, 주로 엑세스 하는 엔티티에 설정하는 것을 추천
     private LocalDateTime orderDate; // java8이상부턴 LocalDateTime을 사용하면 hibernate에서 자동으로 date타입으로 맵핑함
@@ -32,5 +34,17 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문 상태 [ORDER, CANCEL]
 
-
+    // 연관관계 편의 메소드 //
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
