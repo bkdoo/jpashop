1f5021bcfe9334caec5abb70f10ef85114a40f09
bkdoo <kyeongdoos@gmail.com>
2022-06-22 00:08:59 +0900

    20220621 JPA 실습 - 상품 기능 개발 - ItemRepository, ItemService - Item Entity 내부에 비즈니스로직 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,6 +27,19 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // 비즈니스 로직 //
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void minusStock(int quantity) {
+        int restQuantity = this.stockQuantity - quantity;
+        if(restQuantity<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restQuantity;
+    }
     
 
 }
---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void addItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public Item findItem(Long id) {
+        return itemRepository.findOne(id);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+}
